7d70f37e1e2ce1bc3ae91bed4527f493752dc040
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-03-29 12:35:28 -0700

    It will have script which runs all quiz questions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-    $("#quiz_city").hide();
-    $("#quiz_company").hide();
-    $("#quiz_team").hide();
-    $("#quiz_project").hide();
-    $("#quiz_level").hide();
-    $("#quiz_toy").hide();
-    $("#quiz_reason").hide();
-    $("#answers").hide();
-  $("#start").submit(function(event) {
+  $("#quiz_city").hide();
+  $("#quiz_company").hide();
+  $("#quiz_team").hide();
+  $("#quiz_project").hide();
+  $("#quiz_level").hide();
+  $("#quiz_toy").hide();
+  $("#quiz_reason").hide();
+  $("#answers").hide();
+  $("#start").click(function(event) {
     event.preventDefault();
+    $("#text").hide();
     $("#quiz_city").show();
     $("#quiz_company").hide();
     $("#quiz_team").hide();
@@ -17,9 +18,14 @@ $(document).ready(function() {
     $("#quiz_toy").hide();
     $("#quiz_reason").hide();
     $("#answers").hide();
+    $("#quiz_city").submit(function(event) {
+      var city = $("input[name=type]:checked").val();
+      $(".type").text(city);
+    });
   });
-  $("#quiz_city").submit(function(event) {
+  $("#next").click(function(event) {
     event.preventDefault();
+    $("#text").hide();
     $("#quiz_city").hide();
     $("#quiz_company").show();
     $("#quiz_team").hide();
@@ -28,5 +34,111 @@ $(document).ready(function() {
     $("#quiz_toy").hide();
     $("#quiz_reason").hide();
     $("#answers").hide();
+    $("#quiz_company").submit(function(event) {
+      var company = $("input[name=type]:checked").val();
+      $(".type").text(company);
+    });
   });
+  $("#next1").click(function(event) {
+    event.preventDefault();
+    $("#text").hide();
+    $("#quiz_city").hide();
+    $("#quiz_company").hide();
+    $("#quiz_team ").show();
+    $("#quiz_project").hide();
+    $("#quiz_level").hide();
+    $("#quiz_toy").hide();
+    $("#quiz_reason").hide();
+    $("#answers").hide();
+    $("#quiz_team").submit(function(event) {
+      var team = $("input[name=type]:checked").val();
+      $(".type").text(team);
+    });
   });
+  $("#next2").click(function(event) {
+    event.preventDefault();
+    $("#text").hide();
+    $("#quiz_city").hide();
+    $("#quiz_company").hide();
+    $("#quiz_team").hide();
+    $("#quiz_project").show();
+    $("#quiz_level").hide();
+    $("#quiz_toy").hide();
+    $("#quiz_reason").hide();
+    $("#answers").hide();
+    $("#quiz_project").submit(function(event) {
+      var project = $("input[name=type]:checked").val();
+      $(".type").text(project);
+    });
+  });
+  $("#next3").click(function(event) {
+    event.preventDefault();
+    $("#text").hide();
+    $("#quiz_city").hide();
+    $("#quiz_company").hide();
+    $("#quiz_team").hide();
+    $("#quiz_project").hide();
+    $("#quiz_level").show();
+    $("#quiz_toy").hide();
+    $("#quiz_reason").hide();
+    $("#answers").hide();
+    $("#quiz_level").submit(function(event) {
+      var level = $("input[name=type]:checked").val();
+      $(".type").text(level);
+    });
+  });
+  $("#next4").click(function(event) {
+    event.preventDefault();
+    $("#text").hide();
+    $("#quiz_city").hide();
+    $("#quiz_company").hide();
+    $("#quiz_team").hide();
+    $("#quiz_project").hide();
+    $("#quiz_level").hide();
+    $("#quiz_toy").show();
+    $("#quiz_reason").hide();
+    $("#answers").hide();
+    $("#quiz_toy").submit(function(event) {
+      var toy = $("input[name=type]:checked").val();
+      $(".type").text(toy);
+    });
+  });
+  $("#next5").click(function(event) {
+    event.preventDefault();
+    $("#text").hide();
+    $("#quiz_city").hide();
+    $("#quiz_company").hide();
+    $("#quiz_team").hide();
+    $("#quiz_project").hide();
+    $("#quiz_level").hide();
+    $("#quiz_toy").hide();
+    $("#quiz_reason").show();
+    $("#answers").hide();
+    $("#quiz_reason").submit(function(event) {
+      var reason = $("input[name=type]:checked").val();
+      $(".type").text(reason);
+    });
+  });
+    if (company === "Large") {
+      $('#C#').show();
+      $('#Java').hide();
+      $('#Ruby').hide();
+      $('#PHP').hide();
+      $('#CSS').hide();
+      $('#WebDesign').hide();
+    // } else if ( )  {
+    //   $('#C#').hide();
+    //   $('#Java').hide();
+    //   $('#Ruby').hide();
+    //   $('#PHP').hide();
+    //   $('#CSS').hide();
+    //   $('#WebDesign').hide();
+    // } else {
+    //   $('#C#').hide();
+    //   $('#Java').hide();
+    //   $('#Ruby').hide();
+    //   $('#PHP').hide();
+    //   $('#CSS').hide();
+    //   $('#WebDesign').hide();
+    }
+});
